ba462dc433a531ed90596d9bfb2abe9c83917efc
danielerik123 <72527640+danielerik123@users.noreply.github.com>
2020-12-04 13:39:24 +0700

    Add files via upload

---
+++ Jenny.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+#Biodata Park Min Ji 박민지
+
+read nama
+echo "hi, selamat datang" $nama
+read -p "masukan nama kalian sekali lagi :" nama
+echo "...................................."
+echo "selamat datang kawan" $nama
+echo "film ini seru bangat"
+echo ".........................................."
+echo "Atthur : Mr_RB"
+echo "...................................."
+echo "Pemilik Script : Mr_RB"
+echo "...................................."
+echo "Nomer Wa : 0895392705209"
+echo "...................................."
+echo "Gua Suka Bangat Film Ini Tonton Ya"
+echo ".........................."
+echo "Di Bawah Link YT Ya Gays"
+echo ".........................."
+echo "https://youtu.be/oHWrxcLUt1I"
+echo ".............................."
+echo "Itu di atas link Ya ☝☝☝"
+echo "................................"
+echo "Nama: Park Min Ji"
+echo "..................."
+echo "Nama artis: Park Minji"
+echo "......................."
+echo "Negara asal: South Korean"
+echo ".........................."
+echo "Jenis kelamin: perempuan "
+echo "Lahir: July 22, 1989"
+echo "Umur: 29"
+echo "Kunjungin WebSet Ini Aja"
+echo "http://hallo2600.blogspot.com/2018/12/biodata-park-min-ji.html?m=1"
+echo "..............................................."
+echo ""
+echo "(1).exit"
+read -p "masukan pilihan anda di sini :" pilih;
+if [ $pilih = "1" ]
+then
+   echo "exit"
+   echo "terima kasih , udah berkunjung"
+   sleep 1
+fi
